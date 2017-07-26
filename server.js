31121758d51149de528990a14720f66d422d3911
srinivasu619 <srinivasu.618@gmail.com>
2017-07-26 21:44:52 +0530

    Added the main logic

--- server.js
+++ server.js
@@ -6,6 +6,40 @@ app.get('/',function(req,res){
 	res.render('index');
 });
 
+app.get('/:time',function(req,res){
+	const timestamp = req.params.time;
+	if(isNaN(Number(timestamp)))
+	{
+		var data = new Date(timestamp);
+		if (data == 'Invalid Date') {
+			var datedata = {
+			unix_timestamp: "null",
+			date:"null"
+		} 
+      return res.render('time',{data:datedata});
+		}
+    var datedata = {
+			unix_timestamp: Date.parse(data),
+			date:data.getDate()+"-"+(data.getMonth()+1)+"-"+data.getFullYear()
+		}
+		return res.render('time',{data:datedata});
+	}
+	else{
+		var data = new Date(Number(timestamp));
+		if (data == 'Invalid Date') {
+			var datedata = {
+			unix_timestamp: "null",
+			date:"null"
+		} 
+      return res.render('time',{data:datedata});
+		}
+    var datedata = {
+			unix_timestamp: Number(timestamp),
+			date:data.getDate()+"-"+(data.getMonth()+1)+"-"+data.getFullYear()
+		};
+    return res.render('time',{data:datedata});
+	}
+})
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
